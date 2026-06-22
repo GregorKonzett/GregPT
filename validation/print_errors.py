d9f4866dd5da0050bee6d5bcc4fd8b32bd2ad8cb
Greg <gregor.konzett98@gmail.com>
2026-06-22 09:07:05 +0100

    Plot learning rate

--- validation/print_errors.py
+++ validation/print_errors.py
@@ -9,8 +9,27 @@ with open(file, "r") as f:
 training_lines = progress[progress["event"] == "train"]
 validation_lines = progress[progress["event"] == "val"]
 
+# plt.legend()
+#
+# plt.show()
+
+
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('global steps')
+ax1.set_ylabel('learning rate', color=color)
+ax1.plot(training_lines["global_step"], training_lines["lr"], color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('cross-entropy loss', color=color)
 plt.plot(training_lines["global_step"], training_lines["train_loss"], label='Training loss', color='blue', marker='o')
 plt.plot(validation_lines["global_step"], validation_lines["val_loss"], label='Validation loss', color='orange', marker='s')
-plt.legend()
+ax2.tick_params(axis='y', labelcolor=color)
 
+fig.legend()
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
